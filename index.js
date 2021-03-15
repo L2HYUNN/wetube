1242bd835f7ac846a4fe8d1cd08c5a8328f42cdf
L2HYUNN <dhl9810@naver.com>
2021-03-16 00:39:47 +0900

    #2.7 Express Core: Middlewares part Two

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import express from "express";
 // const express = require('express');
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser"; 
+
 const app = express();
 
 const PORT = 4000;
@@ -23,13 +28,15 @@ const handleProfile = (req, res) => res.send("You are on my profile");
 //     res.send("You are on my profile");
 // }
 
-const betweenHome = (req, res, next) => {
- console.log("I'm between");
- next();
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(helmet());
+app.use(morgan("dev"));
 
-};
-
-app.use(betweenHome);
+const middleware = (req, res, next) => {
+    res.send(`not happening`);
+}
 
 app.get("/", handleHome);
 

5e3ff8c49db77d9aa44c2eaeb345f141268edb11
L2HYUNN <dhl9810@naver.com>
2021-03-16 16:25:40 +0900

    #2.9 MVC Pattern part One

--- app.js
+++ app.js
@@ -4,24 +4,13 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser"; 
-import { userRouter } from "./router";
+import userRouter from "./routers/userRouter";
+import globalRouter from "./routers/globalRouter";
 
 const app = express();
 
 // function handleListening(){
 //     console.log(`Listening on http://localhost:${PORT}`);
-// }
-
-const handleHome = (req, res) => res.send("Hello from mymy home");
-
-// function handleHome(req, res) {
-//     res.send("Hello from home");
-// }
-
-const handleProfile = (req, res) => res.send("You are on my profile");
-
-// function handleProfile(req, res) {
-//     res.send("You are on my profile");
 // }
 
 app.use(cookieParser());
@@ -30,12 +19,9 @@ app.use(bodyParser.urlencoded({ extended: true}));
 app.use(helmet());
 app.use(morgan("dev"));
 
-
-app.get("/", handleHome);
-
-app.get("/profile", handleProfile);
-
+app.use("/", globalRouter);
 app.use("/user", userRouter);
+app.use("/video", videoRouter);
 
 export default app;
 

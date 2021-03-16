846add245d34ef49495491f60832c372b9ab294f
L2HYUNN <dhl9810@naver.com>
2021-03-16 23:37:13 +0900

    #2.12 Recap

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,5 +1,12 @@
 import express from "express";
+import { changePassword, editProfile, userDetail, users } from "../controllers/userController";
+import routers from "../routes";
 
 const userRouter = express.Router();
+
+userRouter.get(routers.users, users);
+userRouter.get(routers.userDetail, userDetail);
+userRouter.get(routers.editProfile, editProfile);
+userRouter.get(routers.changePassword, changePassword);
 
 export default userRouter;

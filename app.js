909975bd4a1ce110eea675c4600daf3e4b846512
L2HYUNN <dhl9810@naver.com>
2021-03-17 21:15:10 +0900

    #2.16 Local Variables in Pug

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser"; 
+import { localsMiddleware } from "./middlewares";
+import routes from "./routes"; 
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
-import routes from "./routes"; 
 
 const app = express();
 
@@ -15,12 +16,14 @@ const app = express();
 //     console.log(`Listening on http://localhost:${PORT}`);
 // }
 
+app.use(helmet());
 app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
-app.use(helmet());
 app.use(morgan("dev"));
+
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);

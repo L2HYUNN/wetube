96d0cf172767d82deabb5d50d4d97229b81e41fc
L2HYUNN <dhl9810@naver.com>
2021-03-23 18:33:06 +0900

    #3.1 Connecting to MongoDB +

--- db.js
+++ db.js
@@ -13,5 +13,6 @@ const db = mongoose.connection;
 
 const handleOpen = () => console.log(" ✅ Connected to DB");
 const handleError = (error) => console.log(` ❌ Error on DB connection:${error}`);
+
 db.once("open", handleOpen);
 db.on("error", handleError);

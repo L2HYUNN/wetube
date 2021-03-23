964fa925e99f7e1b693cd5cc57d4face995cdc02
L2HYUNN <dhl9810@naver.com>
2021-03-23 15:04:02 +0900

    #2.25 More Controlers

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,5 @@
 import { videos } from "../db";
+import routes from "../routes";
 
 export const home = (req, res) => {
     res.render("home", { pageTitle : "Home", videos });
@@ -14,8 +15,17 @@ export const search = (req, res) => {
 // export const videos = (req, res) => 
 // res.render("videos", { pageTitle : "Videos"});
 
-export const upload = (req, res) => 
+export const getUpload = (req, res) => {
     res.render("upload", { pageTitle : "Upload"});
+};
+
+export const postUpload = (req, res) => {
+    const {
+        body: { file, title, description }
+    } = req;
+    // To Do: Upload and Save video
+    res.redirect(routes.videoDetail(videos[0].id));
+};
 
 export const videoDetail = (req, res) => 
     res.render("videoDetail", { pageTitle : "Video Detail"});

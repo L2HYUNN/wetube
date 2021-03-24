f2c3bb693e0bf14fc7cdfe1df6fc7194f40ee1c2
L2HYUNN <dhl9810@naver.com>
2021-03-24 15:04:40 +0900

    #3.7 Uploading and Creating a Video part Two

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -28,7 +28,7 @@ export const getUpload = (req, res) => {
 export const postUpload = async (req, res) => {
     const {
         body: { title, description },
-        file: { path}
+        file: { path }
     } = req;
     const newVideo = await Video.create({
         fileUrl: path,

65edf126f6618747e5407d2df55aedbcf42943d1
L2HYUNN <dhl9810@naver.com>
2021-04-23 21:01:12 +0900

    heroku

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,5 +1,5 @@
 const path = require("path");
-// const autoprefixer = require("autoprefixer");
+const autoprefixer = require("autoprefixer");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const MODE = process.env.WEBPACK_ENV;

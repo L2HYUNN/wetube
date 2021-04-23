684870870212cbc73083e1cb156f60e72bbe3907
L2HYUNN <dhl9810@naver.com>
2021-04-23 21:17:56 +0900

    asd

--- src/passport.js
+++ src/passport.js
@@ -1,9 +1,12 @@
+import dotenv from "dotenv";
+dotenv.config();
 import passport from "passport";
 import GithubStrategy from "passport-github";
 import FacebookStrategy from "passport-facebook";
 import { facebookLoginCallback, githubLoginCallback } from "./controllers/userController";
 import User from "./models/User";
 import routes from "./routes";
+
 
 passport.use(User.createStrategy());
 
@@ -17,18 +20,18 @@ passport.use(
     )
 );
 
-// passport.use(
-//     new FacebookStrategy({
-//     clientID: process.env.FB_ID,
-//     clientSecret: process.env.FB_SECRET,
-//     // callbackURL: `http://localhost:4000${routes.facebookCallback}`
-//     callbackURL: `https://c13d5e9f74c2.ngrok.io${routes.facebookCallback}`,
-//     profileFields: ['id', 'displayName', 'photos', 'email'],
-//     scope: ['public_profile', "email"]
-//     },
-//     facebookLoginCallback
-//     )
-// );
+passport.use(
+    new FacebookStrategy({
+    clientID: process.env.FB_ID,
+    clientSecret: process.env.FB_SECRET,
+    // callbackURL: `http://localhost:4000${routes.facebookCallback}`
+    callbackURL: `https://c13d5e9f74c2.ngrok.io${routes.facebookCallback}`,
+    profileFields: ['id', 'displayName', 'photos', 'email'],
+    scope: ['public_profile', "email"]
+    },
+    facebookLoginCallback
+    )
+);
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
